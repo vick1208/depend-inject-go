9eb421e4773bfb526d7f32d55b4b2ed58fef8ec7
Vicky AS <vickyhuang808@gmail.com>
2023-07-06 15:30:16 +0700

    Bind Value

--- simple/injector.go
+++ simple/injector.go
@@ -3,7 +3,11 @@
 
 package simple
 
-import "github.com/google/wire"
+import (
+	"github.com/google/wire"
+	"io"
+	"os"
+)
 
 func InitializedService(isError bool) (*SimpleService, error) {
 	wire.Build(
@@ -42,5 +46,23 @@ var helloSet = wire.NewSet(
 
 func InitializedHelloService() *HelloService {
 	wire.Build(helloSet, NewHelloService)
+	return nil
+}
+
+func InitializedFooBar() *FooBar {
+	wire.Build(NewFoo, NewBar, wire.Struct(new(FooBar), "*"))
+	return nil
+}
+
+var fooValue = &Foo{}
+var barValue = &Bar{}
+
+func InitializedFooBarUsingValue() *FooBar {
+	wire.Build(wire.Value(fooValue), wire.Value(barValue), wire.Struct(new(FooBar), "*"))
+	return nil
+}
+
+func InitializedReader() io.Reader {
+	wire.Build(wire.InterfaceValue(new(io.Reader), os.Stdin))
 	return nil
 }
